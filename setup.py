449a34eb75094a2ab971de7fe9938775b78743f6
Artyom Pervukhin <artyom@evasive.ru>
2013-08-13 17:03:17 +0400

    setup.py file added

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(name='logfeed',
+      version='1.0',
+      description='Read log messages from rotated files',
+      author='Artyom Pervukhin',
+      author_email='artyom@evasive.ru',
+      url='https://github.com/artyom/logfeed',
+      py_modules=['logfeed'],
+     )
